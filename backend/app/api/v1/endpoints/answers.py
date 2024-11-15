97a973fdb1ee4fc70e6d717ad2ff23c969a9407d
Rami Hassan <hsn.rami@gmail.com>
2024-11-15 17:36:52 +0200

    adding the answers endpoints

---
+++ backend/app/api/v1/endpoints/answers.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, HTTPException, Depends, status, Response  # noqa
+
+from app import schemas, services, db
+
+router = APIRouter()
+
+
+@router.post('/', response_model=schemas.answers.AnswerRead, status_code=status.HTTP_201_CREATED)
+async def answer_question(answer: schemas.answers.AnswerCreate, db=Depends(db.session.get_db)):
+    return await services.answers.answer_question(db, answer)
