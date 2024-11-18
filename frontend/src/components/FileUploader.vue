e9e19baeaad79e129192af92d55ceba8733694fc
Rami Hassan <hsn.rami@gmail.com>
2024-11-19 00:55:11 +0200

    enable uploading a file

--- frontend/src/components/FileUploader.vue
+++ frontend/src/components/FileUploader.vue
@@ -1,12 +1,46 @@
 <script lang="ts" setup>
+import { ref, defineProps, useTemplateRef, computed } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-defineProps(['type', 'icon', 'color'])
+const props = defineProps(['type', 'icon', 'color'])
+const accept = computed(() => {
+  switch (props.type) {
+    case 'json':
+      return 'application/json'
+    case 'csv':
+      return 'text/csv'
+    case 'excel':
+      return 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    default:
+      return '*/*'
+  }
+})
+const inputRef = useTemplateRef<HTMLInputElement>('fileInput')
+const file = ref<File | null>()
+
+function onFileChanged($event: Event) {
+  const target = $event.target as HTMLInputElement
+  if (target && target.files) {
+    file.value = target.files[0]
+    // TODO: send to the backend for processing
+  }
+}
+
+function triggerFileUpload() {
+  inputRef.value?.click()
+}
 </script>
 
 <template>
-  <div class="flex flex-col pt-5 cursor-pointer">
+  <div class="flex flex-col pt-5 cursor-pointer" @click="triggerFileUpload">
     <FontAwesomeIcon class="" :icon="icon" size="2xl" :style="[{ color }]" />
     <span class="text-center">{{ type }}</span>
+    <input
+      type="file"
+      ref="fileInput"
+      @change="onFileChanged($event)"
+      style="display: none"
+      :accept="accept"
+    />
   </div>
 </template>
