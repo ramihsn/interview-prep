1170dd3523e67195dc18fb087629882f5b2de906
Rami Hassan <hsn.rami@gmail.com>
2024-11-28 12:09:09 +0200

    [docker][be] remove promtail and start using python-logging-loki

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
 from fastapi.middleware.cors import CORSMiddleware
+from multiprocessing import Queue
 from fastapi import FastAPI
+import logging_loki
+import logging
 
 from app import api
 from app.core import lifespan
+
+handler = logging_loki.LokiQueueHandler(
+    Queue(-1),
+    url="http://loki:3100/loki/api/v1/push",
+    tags={"job": "backend", "host": "backend"},
+    # auth=("username", "password"),
+    version="1",
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),  # Logs to a file
+        logging.StreamHandler(),         # Logs to stdout
+        handler,
+    ],
+)
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
