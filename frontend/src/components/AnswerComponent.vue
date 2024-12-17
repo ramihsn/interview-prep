366fab714ed9b820e4ca811f78f158bd542de2f4
Rami Hassan <hsn.rami@gmail.com>
2024-12-17 19:57:37 +0200

    [fe] enable MD for the answer or the review sections

--- frontend/src/components/AnswerComponent.vue
+++ frontend/src/components/AnswerComponent.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import MarkdownIt from 'markdown-it'
 
 import AnswerCreate from '@/models/AnswerCreate'
 import Answer from '@/models/Answer'
@@ -7,8 +8,17 @@ import Answer from '@/models/Answer'
 const props = defineProps<{ answer: Answer | AnswerCreate }>()
 const emit = defineEmits(['submitAnswer'])
 
-const answerText = ref<string>(props.answer?.answer || '')
-const reviewText = ref<string>(props.answer?.review || '')
+// Markdown Rendering
+const md = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+})
+
+const answerText = ref<string>(md.render(props.answer?.answer) || '')
+const editAnswer = ref(false)
+const reviewText = ref<string>(md.render(props.answer?.review) || '')
+const editReview = ref(false)
 const rating = ref<number | null>(props.answer?.rating ?? null)
 const ratingError = ref(false)
 
@@ -37,10 +47,18 @@ defineExpose({ emitAnswer })
 <template>
   <div class="p-4 space-y-4">
     <!-- Textarea for answer input -->
+    <div
+      v-if="answerText && !editAnswer"
+      v-html="answerText"
+      class="prose"
+      @dblclick="editAnswer = true"
+    ></div>
     <textarea
+      v-else
       v-model="answerText"
       class="textarea textarea-bordered w-full p-4"
       placeholder="Write your answer here..."
+      @keyup.esc="editAnswer = false"
     ></textarea>
 
     <!-- Display the answer details if available -->
@@ -48,10 +66,18 @@ defineExpose({ emitAnswer })
       <!-- Review Section -->
       <div class="space-y-2">
         <div class="font-bold text-lg">Review:</div>
+        <div
+          v-if="reviewText && !editReview"
+          v-html="reviewText"
+          class="prose"
+          @dblclick="editReview = true"
+        ></div>
         <textarea
+          v-else
           v-model="reviewText"
           class="textarea w-full p-4"
           placeholder="Write your review here..."
+          @keyup.esc="editReview = false"
         ></textarea>
       </div>
 
